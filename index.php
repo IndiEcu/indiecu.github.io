b7700547ace7d698ee9a4f2948abc8a1e4bf0f2b
Indi Ecu <indi.ecu@gmail.com>
2025-04-18 08:00:55 -0400

    fix:

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php
 header('HTTP/1.1 503 Service Unavailable');
-header('Retry-After: 3600'); // Время в секундах (например, 1 час = 3600)
+header('Retry-After: 3600'); // Время в секундах (например, 1 час =
+echo "ok";
